3dbc50e7b23a1b6feff1dc42a3894336faaa12dc
Md Rafiqul Islam <vwaaala@gmail.com>
2024-02-27 05:24:15 +0600

    Update Ticket.php

--- src/Models/Ticket.php
+++ src/Models/Ticket.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Bunker\SiteContact\Models;
+namespace Bunker\SupportTicket\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
